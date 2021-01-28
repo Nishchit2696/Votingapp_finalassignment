f6463986f59f6ab5ef768d7069cc0abdc123a557
Nishchit2696 <renish.shah8@gmail.com>
2021-01-28 14:32:47 +0545

    SpleashScreen

---
+++ app/src/main/java/com/example/voting/Splash.kt
@@ -0,0 +1,28 @@
+package com.example.voting
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        CoroutineScope(Dispatchers.IO).launch {
+            delay(1000)
+            startActivity(
+                Intent(
+                this@Splash,
+                login::class.java
+            )
+            )
+            finish()
+        }
+
+    }
+}
