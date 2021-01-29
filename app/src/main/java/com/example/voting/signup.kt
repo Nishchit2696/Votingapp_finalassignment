8d71a8bfb6c5e1a24bd1b3f5b55c2bbc8c401ea9
Nishchit2696 <renish.shah8@gmail.com>
2021-01-29 15:53:49 +0545

    register instant

--- app/src/main/java/com/example/voting/signup.kt
+++ app/src/main/java/com/example/voting/signup.kt
@@ -1,11 +1,23 @@
 package com.example.voting
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import kotlin.math.log
 
 class signup : AppCompatActivity() {
+
+    private lateinit var log : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_signup)
+
+        log = findViewById(R.id.log)
+        log.setOnClickListener{
+            val intent = Intent(this, login::class.java)
+            startActivity(intent)
+        }
     }
 }
