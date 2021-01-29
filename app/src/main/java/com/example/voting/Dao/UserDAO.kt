961efc319e3331dc792a555093c97b9a9cd4da5c
Nishchit2696 <renish.shah8@gmail.com>
2021-01-29 16:57:58 +0545

    Dao error fixed

--- app/src/main/java/com/example/voting/Dao/UserDAO.kt
+++ app/src/main/java/com/example/voting/Dao/UserDAO.kt
@@ -4,6 +4,7 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Update
+import com.example.voting.entity.User
 
 
 @Dao
@@ -16,4 +17,3 @@ interface UserDAO {
     suspend fun updateUser(user: User)
 }
 
-}
---
+++ app/src/main/java/com/example/voting/Userdb/UserDB.kt
@@ -0,0 +1,4 @@
+package com.example.voting.Userdb
+
+class UserDB {
+}
