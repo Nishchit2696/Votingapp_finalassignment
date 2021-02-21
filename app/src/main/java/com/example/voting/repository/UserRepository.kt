a9ed206c60933115ea40cfb3fd09ed2c69f5eabb
Nishchit2696 <renish.shah8@gmail.com>
2021-02-22 00:00:52 +0545

    UserRepository

---
+++ app/src/main/java/com/example/voting/repository/UserRepository.kt
@@ -0,0 +1,28 @@
+package com.example.voting.repository
+
+import com.example.voting.api.MyApiRequest
+import com.example.voting.api.ServiceBuilder
+import com.example.voting.api.UserAPI
+import com.example.voting.entity.User
+import com.example.voting.response.LoginResponse
+
+class UserRepository
+    : MyApiRequest() {
+
+    private val userAPI =
+        ServiceBuilder.buildService(UserAPI::class.java)
+
+    //register User
+
+    suspend fun registerUser(user: User): LoginResponse {
+        return apiRequest {
+            userAPI.registerUser(user)
+        }
+    }
+    //login user
+    suspend fun checkUser(username:String,password:String):LoginResponse{
+        return apiRequest {
+            userAPI.checkUser(username,password)
+        }
+    }
+}
