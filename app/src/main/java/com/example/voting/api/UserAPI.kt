c536117989a984a6a2fb9da093949e6ffa64585f
Nishchit2696 <renish.shah8@gmail.com>
2021-02-22 00:14:42 +0545

    UserApi

--- app/src/main/java/com/example/voting/api/UserAPI.kt
+++ app/src/main/java/com/example/voting/api/UserAPI.kt
@@ -1,4 +1,25 @@
 package com.example.voting.api
 
+import com.example.voting.entity.User
+import com.example.voting.response.LoginResponse
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface UserAPI {
+
+    @POST("/User_insert")
+    suspend fun registerUser(
+        @Body user: User
+    ): Response<LoginResponse>
+
+    //Login user
+    @FormUrlEncoded
+    @POST("/login")
+    suspend fun checkUser(
+        @Field("username") username : String,
+        @Field("password") password : String
+    ): Response<LoginResponse>
 }
