4cbeeb974644a4b4fa3f8c40a3f07974643fb7e6
Nishchit2696 <renish.shah8@gmail.com>
2021-03-01 09:33:36 +0545

    passport entity

--- app/src/main/java/com/example/voting/entity/Passport.kt
+++ app/src/main/java/com/example/voting/entity/Passport.kt
@@ -1,4 +1,13 @@
 package com.example.voting.entity
 
-class Passport {
-}
+data class Passport (
+        val _id : String? = null,
+        val simage : String? = null,
+        val Firstname : String? = null,
+        val Lastname : String? = null,
+        val Fathername: String?=null,
+        val CitizenshipNo : String? = null,
+        val Ocupation : String? = null,
+        val Education : String? = null,
+        val Phonenumber : String? = null
+        )
